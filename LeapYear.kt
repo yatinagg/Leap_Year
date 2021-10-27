c39fee8a804aa4542418645858f2edcbc13ebb6a
yatinagg <yatinagg12@gmail.com>
2021-10-27 11:17:33 +0530

    Leap year Kotlin

--- LeapYear.kt
+++ LeapYear.kt
@@ -1,18 +1,24 @@
 import java.util.*
 
+
+/**
+ * main function
+ */
 fun main() {
     val num1 = Scanner(System.`in`)
     val year = num1.nextInt()
     val leapYear = isLeapYear(year)
-    if(leapYear)
+    if (leapYear)
         println("$year is a leap year")
     else
         println("$year is not a leap year")
 
 }
 
-fun isLeapYear(year: Int): Boolean {
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-        return true
-    return false
-}
+
+/**
+ * function to check leap year
+ * @param year the year to be checked
+ * @return Boolean leap year or not
+ */
+fun isLeapYear(year: Int) = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
